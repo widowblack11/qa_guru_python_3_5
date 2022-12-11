b10b35e71aeae610d6343c6bcaaf91980771cb2e
widowblack11 <oksana.prokopenko16@gmail.com>
2022-12-11 19:43:20 +0300

    added comments to tests

--- tests/test_demoqa.py
+++ tests/test_demoqa.py
@@ -5,26 +5,28 @@ from selene.support.shared import browser
 
 
 def test_sending_practice_form(open_browser):
-    browser.element('#firstName').should(be.blank).type('Oksana')
-    browser.element('#lastName').should(be.blank).type('Prokopenko')
-    browser.element('#userEmail').should(be.blank).type('proyeillepebe-2165@yopmail.com')
-    browser.element('[for="gender-radio-2"]').click()
-    browser.element('#userNumber').should(be.blank).type('1234567890')
-    browser.element('#dateOfBirthInput').click()
-    browser.element('.react-datepicker__year-select').click()
-    browser.element('.react-datepicker__year-select [value="1998"]').click()
-    browser.element('.react-datepicker__month-select').click()
-    browser.element('.react-datepicker__month-select [value="0"]').click()
-    browser.element('.react-datepicker__day--005').click()
-    browser.element('#subjectsInput').should(be.blank).type('arts').press_enter()
-    browser.element('[for=hobbies-checkbox-1').click()
-    browser.element('[for=hobbies-checkbox-3').click()
+    browser.element('#firstName').should(be.blank).type('Oksana') #ввод имени
+    browser.element('#lastName').should(be.blank).type('Prokopenko') #ввод фамилии
+    browser.element('#userEmail').should(be.blank).type('proyeillepebe-2165@yopmail.com') #вод эл почты
+    browser.element('[for="gender-radio-2"]').click() #выбор гендера
+    browser.element('#userNumber').should(be.blank).type('1234567890') # ввод номера телефонв
+    browser.element('#dateOfBirthInput').click() #открытие формы выбора даты рождения
+    browser.element('.react-datepicker__year-select').click() #открытие выбора года рождения
+    browser.element('.react-datepicker__year-select [value="1998"]').click() # выбор года рождения
+    browser.element('.react-datepicker__month-select').click() #открытие формы выбора месяца рождения
+    browser.element('.react-datepicker__month-select [value="0"]').click() #выбор месяца рождения
+    browser.element('.react-datepicker__day--005').click() #выбор дня рождения
+    browser.element('#subjectsInput').should(be.blank).type('arts').press_enter() #ввод предмета
+    browser.element('[for=hobbies-checkbox-1').click() #выбор хобби
+    browser.element('[for=hobbies-checkbox-3').click() #выбор хобби
     browser.element('#uploadPicture').set_value(
-            os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir, 'file/test_5.jpeg')))
-    browser.element('#currentAddress').should(be.blank).type('Краснодарский край')
-    browser.element('#react-select-3-input').type('NCR').press_enter()
-    browser.element('#react-select-4-input').type('Noida').press_enter()
-    browser.element('#submit').press_enter()
+            os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir, 'file/test_5.jpeg'))) #загрузка файла
+    browser.element('#currentAddress').should(be.blank).type('Краснодарский край') #ввод адрес
+    browser.element('#react-select-3-input').type('NCR').press_enter() #выбор штата
+    browser.element('#react-select-4-input').type('Noida').press_enter() #выбор города
+    browser.element('#submit').press_enter() #отправка формы
+
+    #проверка соответсвия отпарвленных данных в форме и полученных в итоговой таблицы после отправки заполненной формы регистрации
     browser.element('#example-modal-sizes-title-lg').should(have.text('Thanks for submitting the form'))
     browser.element('.table').should(have.text(
     'Oksana' and
